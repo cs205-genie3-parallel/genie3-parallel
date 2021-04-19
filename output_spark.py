27c3fa5ea7eee0ff8fde1fa40631a654a1bab3da
admin <tangjiahui0610@gmail.com>
2021-04-19 20:07:29 +0800

    update spark to output edge and vertices

--- output_spark.py
+++ output_spark.py
@@ -11,4 +11,8 @@ text_file = sc.textFile("ranking_idx.txt")
 
 
 # consider gene link > 0.02 as significant and print them out as edge file
-text_file.filter(lambda line: float(line.split("\t")[2])> 0.02).repartition(1).saveAsTextFile("graph_edges.txt")
+text_file.filter(lambda line: float(line.split("\t")[2])> 0.02) \
+	.repartition(1).saveAsTextFile("graph_edges.txt")
+
+# print all distinct value in the first two columns as vertices file
+text_file.flatMap(lambda x: x.split("\t")[:-1]).distinct().repartition(1).saveAsTextFile("vertices.txt")
