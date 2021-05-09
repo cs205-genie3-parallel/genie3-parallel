d2a2e2a95b5f0b2e26c4b46342bae807df74c903
admin <tangjiahui0610@gmail.com>
2021-05-09 19:34:57 +0800

    update pyspark graph

--- graph.py
+++ graph.py
@@ -1,41 +1,42 @@
 # launch pyspark with:
-# pyspark --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11
+# pyspark --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11 --repositories https://repos.spark-packages.org# 
 
 import graphframes as GF
 from pyspark.sql import SQLContext
-from pyspark import SparkConf, SparkContext
-import pyspark.sql.functions as sf
-from pyspark.sql import SparkSession
-
-conf = SparkConf().setMaster('local[2]').setAppName('graph')
-sc = SparkContext(conf = conf)
-spark = SparkSession(sc)
 sql_context = SQLContext(sc)
 
-
-vertice_df = spark.read.csv(
-    "vertices.csv", 
+vertice_df = sql_context.read.csv(
+    "vertices.csv/part-00000", 
     header=False
 ).toDF("id")
 
 
-edge_df = spark.read.csv(
-    "graph_edges.csv", 
-    header=False
-).toDF("src","dst","relationship")
+edge_df = sql_context.read.csv(
+    "graph_edges.csv/part-00000", 
+    header=False,
+    sep = ','
+).toDF("src", "dst", "relationship")
 
 g = GF.GraphFrame(vertice_df, edge_df) 
 
 g.vertices.write.parquet("vertices")
 g.edges.write.parquet("edges")
 
+# Load the vertices and edges.
+# v = sqlContext.read.parquet("hdfs://./vertices")
+# e = sqlContext.read.parquet("hdfs://./edges")
+# Create a graph
+# g = GF.GraphFrame(v, e)
 
 # Exploring the Graph
 g.vertices.show()
-
 g.edges.show()
 
 vertexInDegrees = g.inDegrees
 vertexInDegrees.show()
 vertexOutDegrees = g.outDegrees
-vertexOutDegrees.show()
+
+
+## explore network by group by or filtering 
+g.edges.filter("relationship > 0.008").count()
+g.edges.filter("relationship > 0.008").show()
